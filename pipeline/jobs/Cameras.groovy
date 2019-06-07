43dbc17b4f9530cc0b4ea595bb2dc9aae2f97f0a
= <barnabas.vizy@gmail.com>
2019-06-06 22:56:11 -0400

    Init commit

--- pipeline/jobs/Cameras.groovy
+++ pipeline/jobs/Cameras.groovy
@@ -5,23 +5,22 @@ currentBuild.displayName = "Cameras Pipeline [ " + currentBuild.number + " ]"
 pipeline {
     agent any
     stages {
+        stage("Stop Container"){
+            steps{
+                sh('sudo docker stop cameras')
+            }
+        }
         stage("Build Docker Container"){
             steps{
-                script{
-
-                    sh ('''
-                        echo $USER
-                        cd /home/barney/docker/cameras
-                        sudo docker build -t=cameras .
-                    ''')
-                }
+                sh ('''
+                    cd /home/barney/docker/cameras
+                    sudo docker build -t=cameras .
+                ''')
             }
         }
         stage("Deploy Docker Container"){
             steps{
-                script{
-                    echo "Test 1"
-                }
+                sh('sudo docker run -d -p 80:6000 --name cameras cameras')
             }
         }
     }
