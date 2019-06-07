6a66e7360a86f28de5f9292c1328c51b4032499c
= <barnabas.vizy@gmail.com>
2019-06-06 22:22:51 -0400

    Init commit

--- pipeline/jobs/Cameras.groovy
+++ pipeline/jobs/Cameras.groovy
@@ -5,22 +5,18 @@ pipeline {
     stages {
         stage("Build Docker Container"){
             steps{
-                step{
-                    script{
-                        sh ('''
-                            cd /home/barney/docker/cameras
-                            docker build -t=cameras
-                        ''')
-                    }
+                script{
+                    sh ('''
+                        cd /home/barney/docker/cameras
+                        docker build -t=cameras
+                    ''')
                 }
             }
         }
         stage("Deploy Docker Container"){
             steps{
-                step{
-                    script{
-                        echo "Test 1"
-                    }
+                script{
+                    echo "Test 1"
                 }
             }
         }
