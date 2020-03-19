16091a2aee404c2c356de9cbd757d5dda85642c7
Vizzyy <barnabas.vizy@gmail.com>
2020-03-19 12:00:02 -0400

    Init DDNS job

--- pipeline/jobs/DDNS.groovy
+++ pipeline/jobs/DDNS.groovy
@@ -2,6 +2,7 @@
 import groovy.json.JsonSlurper
 JsonSlurper slurper = new JsonSlurper()
 
+def IP_ADDRESS;
 
 currentBuild.displayName = "DDNS Pipeline [ " + currentBuild.number + " ]"
 
@@ -12,9 +13,26 @@ pipeline {
         disableConcurrentBuilds()
     }
     stages {
-        stage("Execute") {
+        stage("Grab IP") {
             steps {
                 script {
+
+                    withCredentials([string(credentialsId: 'ddns', variable: 'DDNS')]) {
+                        String ret = sh(script: "curl $DDNS | grep Address", returnStdout: true)
+                    }
+
+                    IP_ADDRESS = ret.split('Address:')[1]
+
+                    echo "DDNS value: $IP_ADDRESS"
+                }
+            }
+        }
+
+        stage("Configure") {
+            steps {
+                script {
+
+//                    sh "aws ec2 delete-security-group --group-name SSHgroup"
 
                     // Create a security group
                     String ret = sh(script: """aws ec2 create-security-group --group-name SSHgroup --description "Security group defining SSH ingress source IPs" """, returnStdout: true)
@@ -23,9 +41,6 @@ pipeline {
                     def securityGroup = parsedJson.GroupId
 
 
-
-
-                    sh "aws ec2 delete-security-group --group-name SSHgroup"
 
                 }
             }
