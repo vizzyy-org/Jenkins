442fec687289f7aeff3fe699c443bb53fb0bf414
Vizzyy <barnabas.vizy@gmail.com>
2020-03-19 11:39:39 -0400

    Init DDNS job

---
+++ pipeline/jobs/DDNS.groovy
@@ -0,0 +1,34 @@
+#! groovy
+
+currentBuild.displayName = "DDNS Pipeline [ " + currentBuild.number + " ]"
+
+pipeline {
+    agent any
+    options {
+        buildDiscarder(logRotator(numToKeepStr:'10'))
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage("Execute") {
+            steps {
+                script {
+
+                    sh "echo hello world"
+
+                }
+            }
+        }
+    }
+    post {
+        success {
+            script {
+                sh "echo SUCCESS"
+            }
+        }
+        failure {
+            script {
+                sh "echo FAILURE"
+            }
+        }
+    }
+}
