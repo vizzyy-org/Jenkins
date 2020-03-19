0a43fc2a5453efd8f3b907590cd2036314a79c01
Vizzyy <barnabas.vizy@gmail.com>
2020-03-19 12:20:22 -0400

    Init DDNS job

--- pipeline/jobs/DDNS.groovy
+++ pipeline/jobs/DDNS.groovy
@@ -2,7 +2,7 @@
 import groovy.json.JsonSlurper
 JsonSlurper slurper = new JsonSlurper()
 
-def IP_ADDRESS;
+def IP_ADDRESS
 
 currentBuild.displayName = "DDNS Pipeline [ " + currentBuild.number + " ]"
 
@@ -12,6 +12,7 @@ pipeline {
         buildDiscarder(logRotator(numToKeepStr:'10'))
         disableConcurrentBuilds()
     }
+    triggers{ cron('H/60 * * * *') }
     stages {
         stage("Grab IP") {
             steps {
@@ -35,16 +36,22 @@ pipeline {
             steps {
                 script {
 
-                    // Delete existing group
-                    sh "aws ec2 delete-security-group --group-name SSHgroup"
+                    withCredentials([string(credentialsId: 'mainEC2', variable: 'INSTANCE_ID'), string(credentialsId: 'WebServerSG', variable: 'WEB_SERVER_SG')]) {
+                        // Delete existing group
+                        sh "aws ec2 delete-security-group --group-name SSHgroup"
 
-                    // Create new security group
-                    String ret = sh(script: """aws ec2 create-security-group --group-name SSHgroup --description "Security group defining SSH ingress source IPs" """, returnStdout: true)
-                    echo "$ret"
-                    Map parsedJson = slurper.parseText(ret)
-                    def securityGroup = parsedJson.GroupId
+                        // Create new security group
+                        String ret = sh(script: """aws ec2 create-security-group --group-name SSHgroup --description "Security group defining SSH ingress source IPs" """, returnStdout: true)
+                        echo "$ret"
+                        Map parsedJson = slurper.parseText(ret) as Map
+                        def securityGroup = parsedJson.GroupId
 
+                        // Configure the singular IP from DDNS value
+                        sh "aws ec2 authorize-security-group-ingress --group-name SSHgroup --protocol tcp --port 22 --cidr $IP_ADDRESS/32"
 
+                        // Attach to EC2
+                        sh """aws ec2 modify-instance-attribute --instance-id $INSTANCE_ID --groups "$WEB_SERVER_SG" "$securityGroup" """
+                    }
 
                 }
             }
