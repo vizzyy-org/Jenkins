9815c085fb293697519c5e007b7886e9669c4684
Vizzyy <barnabas.vizy@gmail.com>
2020-11-03 21:50:25 -0500

    Move hosts to config file

--- pipeline/jobs/metrics.groovy
+++ pipeline/jobs/metrics.groovy
@@ -2,18 +2,7 @@
 
 currentBuild.displayName = "Metrics Pipeline [$currentBuild.number]"
 
-HOSTS = [
-        'pi@lights.local',
-        'pi@summer.local',
-        'pi@vox.local',
-        'pi@battery.local',
-        'pi@octopi.local',
-        'pi@winter1.local',
-        'barney@dinkleberg.local',
-        'pi@herbivore.local',
-        'pi@carnivore.local',
-        'pi@four.local'
-        ]
+HOSTS = hosts.lan_hosts
 
 pipeline {
     agent any
---
+++ vars/hosts.groovy
@@ -0,0 +1,12 @@
+def lan_hosts = [
+                'pi@lights.local',
+                'pi@summer.local',
+                'pi@vox.local',
+                'pi@battery.local',
+                'pi@octopi.local',
+                'pi@winter1.local',
+                'barney@dinkleberg.local',
+                'pi@herbivore.local',
+                'pi@carnivore.local',
+                'pi@four.local'
+        ]
