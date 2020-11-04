52f8f1368c9ae1c6e368dcd2830a932308b0a123
Vizzyy <barnabas.vizy@gmail.com>
2020-11-03 21:55:01 -0500

    Move hosts to config file

--- vars/hosts.groovy
+++ vars/hosts.groovy
@@ -1,12 +1,13 @@
-def lan_hosts = [
-                'pi@lights.local',
-                'pi@summer.local',
-                'pi@vox.local',
-                'pi@battery.local',
-                'pi@octopi.local',
-                'pi@winter1.local',
-                'barney@dinkleberg.local',
-                'pi@herbivore.local',
-                'pi@carnivore.local',
-                'pi@four.local'
-        ]
+lan_hosts = [
+        'pi@lights.local',
+        'pi@summer.local',
+        'pi@vox.local',
+        'pi@battery.local',
+        'pi@octopi.local',
+        'pi@winter1.local',
+        'barney@dinkleberg.local',
+        'pi@herbivore.local',
+        'pi@carnivore.local',
+        'pi@four.local',
+        'ec2-user@vizzyy.com'
+]
