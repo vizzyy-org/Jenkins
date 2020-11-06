3902708b75f9267345d1ad66a14816429b860718
Vizzyy <barnabas.vizy@gmail.com>
2020-11-06 00:42:41 -0500

    Move hosts to config file

--- vars/hosts.groovy
+++ vars/hosts.groovy
@@ -1,16 +1,15 @@
 import groovy.transform.Field
-import groovyjarjarantlr.collections.List
 
 @Field
-def hosts = ['pi@lights.local',
-             'pi@summer.local',
-             'pi@vox.local',
-             'pi@battery.local',
-             'pi@octopi.local',
-             'pi@winter1.local',
-             'barney@dinkleberg.local',
-             'pi@herbivore.local',
-             'pi@carnivore.local',
-             'pi@four.local',
-             'ec2-user@vizzyy.com'
-    ] as List
+java.util.List<java.lang.String> hosts = ['pi@lights.local',
+                                          'pi@summer.local',
+                                          'pi@vox.local',
+                                          'pi@battery.local',
+                                          'pi@octopi.local',
+                                          'pi@winter1.local',
+                                          'barney@dinkleberg.local',
+                                          'pi@herbivore.local',
+                                          'pi@carnivore.local',
+                                          'pi@four.local',
+                                          'ec2-user@vizzyy.com'
+    ]
