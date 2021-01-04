72c14daaa20b2d32dc7361ddc9979196d294a638
Vizzyy <barnabas.vizy@gmail.com>
2021-01-03 23:24:42 -0500

    Remote command

---
+++ pipeline/jobs/remote_command.groovy
@@ -0,0 +1,65 @@
+#! groovy
+
+@Library('main') _
+
+currentBuild.displayName = "Remote Command Pipeline [$currentBuild.number]"
+
+pipeline {
+    agent any
+    options {
+        buildDiscarder(logRotator(numToKeepStr:'10'))
+        disableConcurrentBuilds()
+    }
+    parameters {
+        text(name: 'TARGET_HOSTS', defaultValue: 'pi@lights.local,pi@summer.local,pi@vox.local,pi@battery.local,pi@octopi.local,pi@winter1.local,barney@dinkleberg.local,pi@herbivore.local,pi@carnivore.local,pi@four.local,ec2-user@vizzyy.com,pi@sensor.local,pi@tree.local,2011mbp@2011mbp.local', description: '')
+        text(name: 'REMOTE_COMMAND', defaultValue: 'echo "Hello world"', description: '')
+    }
+    stages {
+        stage('Dynamic Stages') {
+            steps {
+                script {
+                    doDynamicParallelSteps()
+                }
+            }
+        }
+    }
+    post {
+        success {
+            script {
+                sh "echo SUCCESS"
+            }
+        }
+        failure {
+            script {
+                sh "echo FAILURE"
+            }
+        }
+    }
+}
+
+def doDynamicParallelSteps(){
+    def tasks = [:]
+    def HOSTS = $TARGET_HOSTS
+    HOSTS = HOSTS.split(',')
+    echo "$HOSTS"
+
+    for (f in HOSTS) {
+        def host = "${f}"
+        tasks["$host"] = {
+            hostStatus = null
+            stage("$host") {
+                String cmd = """
+                        $REMOTE_COMMAND
+                    """
+                def hostStatus = sh(script: "ssh  -o ConnectTimeout=3 $host '$cmd'", returnStatus: true)
+                if (hostStatus == 255) {
+                    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+                        sh "exit 1"
+                    }
+                }
+            }
+        }
+    }
+
+    parallel tasks
+}
